67eccb179f7b81dbe02ef06afabf97119e8824df
Devin <devin.m.bushey@gmail.com>
2021-11-05 09:13:09 -0700

    unhide secret key

--- website/website/settings.py
+++ website/website/settings.py
@@ -34,7 +34,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+#SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+SECRET_KEY = 'NPh~@MVP|>Csks&8Y?k)+2v<_dh*)@okh+10_}ch;+cbm-*7Tb'
+#NPh~@MVP|>Csks&8Y?k)+2v<_dh*)@okh+10_}ch;+cbm-*7Tb
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
