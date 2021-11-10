3d4dfef8cf80d54ba743c463bf997e158f45b221
Devin <devin.m.bushey@gmail.com>
2021-11-09 19:31:43 -0800

    changed secret key name

--- website/website/settings.py
+++ website/website/settings.py
@@ -36,7 +36,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
-SECRET_KEY = 'NPh~@MVP|>Csks&8Y?k)+2v<_dh*)@okh+10_}ch;+cbm-*7Tb'
+DJANGO_SECRET_KEY = 'NPh~@MVP|>Csks&8Y?k)+2v<_dh*)@okh+10_}ch;+cbm-*7Tb'
 #NPh~@MVP|>Csks&8Y?k)+2v<_dh*)@okh+10_}ch;+cbm-*7Tb
 #.)Z%Q6rX;lhmQT<~ZQIIu1I<jG2.2WhXAH{Wba0i.wwgDrONI}
 #SECRET_KEY = config('DJANGO_SECRET_KEY')
